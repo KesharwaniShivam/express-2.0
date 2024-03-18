b867575933088eb759f38cae44f6b14ec02be69b
KesharwaniShivam <shivamkesharwani6767@gmai.>
2024-03-19 02:01:53 +0530

    database connected

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import path from "path";
+import { registerdata } from "./form.model.js";
+import { config } from "dotenv";
 import mongoose from "mongoose";
 
+
 const app = express();
-const port = 3000;
+// const port = 3000;
 
-const pathloc = path.join(path.resolve(), "views");
+config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+    dbName : "backend",
+}).then(()=>console.log("Database connected")).catch((e)=> console.log(e))
 
 
+// const pathloc = path.join(path.resolve(), "views");
+
+//  MIDDLEWARES 
+
+app.use(express.urlencoded({extended: true}));
 
 
+app.get("/", async(req, res)=>{
+    
+    // console.log(pathloc)
+   await registerdata.create({name : "shivam", email : "s@gmail.com", password : "shdfsdlkf"})
+   res.send("done")
 
-app.get("/", (req, res)=>{
-    res.send("hey")
-    console.log(pathloc)
 })
 
+app.get("/register", (req, res)=>{
+    res.render("register")
+})
+
+app.get("/added", (req, res)=>{
+    res.render("added")
+})
+
+app.post("/register", async(req, res)=>{
+    const {name , email, password} = req.body;
+
+   await registerdata.create({
+        name:name,
+        email: email,
+        password: password,
+    })
+
+    res.redirect("/added")
+})
 // we have to set engine
 app.set("view engine" , "ejs")
 
@@ -23,6 +56,6 @@ app.get("/ejs", (req, res)=>{
     res.render("index", {msg : "hello from index.js"})
 })
 
-app.listen(port, ()=>{
-    console.log(`app is listening at port ${port}`)
+app.listen(process.env.PORT, ()=>{
+    console.log(`app is listening at port ${process.env.PORT}`)
 })
